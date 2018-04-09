bad9c55b73c2532394b82a2156086b34eea558b0
DragonDriver <longjiquan1998@163.com>
2018-04-09 23:49:55 +0800

    task1基本完成

--- Exp2/task1.asm
+++ Exp2/task1.asm
@@ -12,22 +12,22 @@ goods_name DB 11
           DB 11 DUP(0)
 APR DW 0
 AUTH DB 0
-COST1 DW 0;成本
-PROFIT1 DW 0;利润
-COST2 DW 0;成本
-PROFIT2 DW 0;利润
+COST1 DW 0;??
+PROFIT1 DW 0;??
+COST2 DW 0;??
+PROFIT2 DW 0;??
 cycle_times dw 1000
-BNAME DB 'LONG JQ',3 DUP(0);老板姓名
-BPASS DB 'NOPASS';密码
+BNAME DB 'LONG JQ',3 DUP(0);????
+BPASS DB 'NOPASS';??
 N EQU 30
 goods_offset dw 0
-SHOP1 DB 'SHOP1',0;网店名称，0结束
+SHOP1 DB 'SHOP1',0;?????0??
 ga1 DB 'PEN',7 DUP(0)
     DW 35,56,2000,0,?
 ga2 DB 'BOOK',6 DUP(0)
     DW 12,30,2000,0,?
 gaN DB N-2 DUP('TEMP-VALUE',15,0,20,0,0d0H,7,0,0,?,?)
-SHOP2 DB 'SHOP2',0;网店名称，0结束
+SHOP2 DB 'SHOP2',0;?????0??
 gb1 DB 'PEN',7 DUP(0)
     DW 35,50,2000,0,?
 gb2 DB 'BOOK',6 DUP(0)
@@ -58,24 +58,28 @@ START:
             MOV DS,AX
 
 FUNCION1:
-            MOV SI,0;计数器
-            MOV CX,10;计数器
+            MOV SI,0;???
+            MOV CX,6;???
 
-            SET_ALL_ZERO:MOV AL,0
-            MOV in_name[SI+2],AL
+            SET_PWD_ALL_ZERO:
+            MOV AL,0
             MOV in_pwd[SI+2],AL
             INC SI
             DEC CX
-            CMP SI,6
-            JNE SET_ALL_ZERO
+            JNZ SET_PWD_ALL_ZERO
+            
+            MOV SI,0
+            MOV CX,10
 
-            SET_NAME_ALL_ZERO:MOV AL,0
+            SET_NAME_ALL_ZERO:
+            MOV AL,0
             MOV in_name[SI+2],AL
             INC SI
             DEC CX
             JNZ SET_NAME_ALL_ZERO
 
-            INPUT_NAME:LEA DX,INPUT_NAME_MSG
+            INPUT_NAME:
+            LEA DX,INPUT_NAME_MSG
             MOV AH,9
             INT 21H
             LEA DX,in_name
@@ -89,7 +93,8 @@ FUNCION1:
             CMP AL,in_name[3]
             JE AUTH_FAIL
 
-            INPUT_PWD:LEA DX,INPUT_PASS_MSG
+            INPUT_PWD:
+            LEA DX,INPUT_PASS_MSG
             MOV AH,9
             INT 21H
             LEA DX,in_pwd
@@ -99,20 +104,19 @@ FUNCION1:
             AND SI,00FFH
             MOV in_pwd[SI+2],0
 
-            MOV SI,0;计数器
-            MOV CX,10;计数器
+            MOV SI,0;???
+            MOV CX,6;???
 
 FUNCTION2_PWD:
-            MOV AL,in_name[SI+2]
-            CMP AL,BNAME[SI]
-            JNE PRINT_LOGIN_FAILED
             MOV AL,in_pwd[SI+2]
             CMP AL,BPASS[SI]
             JNE PRINT_LOGIN_FAILED
             INC SI
             DEC CX
-            CMP SI,6
-            JNE FUNCTION2_PWD
+            JNZ FUNCTION2_PWD
+            
+            MOV SI,0
+            MOV CX,10
 
 FUNCTION2_NAME:
             MOV AL,in_name[SI+2]
@@ -139,74 +143,84 @@ AUTH_FAIL:
             JNE INPUT_PWD
             MOV BH,0
             MOV BYTE PTR AUTH,BH
-            JMP FUNCTION3
+            JMP START_CYCLES
 
 PRINT_LOGIN_FAILED:
             LEA DX,LOGIN_FAILED_MSG
             MOV AH,9
             INT 21H
             JMP FUNCION1
-;此行之前为功能1，2相关代码
-;此行之后为功能3相关代码
+;???????1?2????
+;???????3????
 START_CYCLES:
-mov cx,word ptr ga1[14];循环次数
+mov cx,10
+mov si,0
+reset_goodsname:
+    mov goods_name[si+2],al
+    inc si
+    dec cx
+    jnz reset_goodsname
+    
+LEA DX,INPUT_GOODS_NAME_MSG
+MOV AH,9
+INT 21H
+LEA DX,goods_name
+MOV AH,10
+INT 21H
+MOV SI, WORD PTR goods_name[1]
+AND SI,00FFH
+mov goods_name[SI+2],0
+
+mov cx,30
+SET_SOLD_NUM_ZERO:
+mov si,cx
+dec si
+imul si,20
+mov word ptr ga1[si+16],0
+mov word ptr gb1[si+16],0
+dec cx
+jnz SET_SOLD_NUM_ZERO
+
+mov cx,word ptr ga1[14];????
 mov cycle_times,cx
 mov ax,0
 call TIMER
 FUNCTION3:
-            PEN_IN_SHOP1:
-            mov ax,word ptr ga1[14];进货总数
-            cmp ax,word ptr ga1[16];判断进货总数是否大于已售数量
-            jle GOODS_SOLD_OUT
-            add word ptr ga1[16],word ptr 1
-            PEN_IN_SHOP2:
-            mov ax,word ptr gb1[14]
-            cmp ax,word ptr gb1[16]
-            jle BOOK_IN_SHOP1
-            add word ptr gb1[16],word ptr 1
-
-            BOOK_IN_SHOP1:
-            mov ax,word ptr ga2[14];进货总数
-            cmp ax,word ptr ga2[16];判断进货总数是否大于已售数量
-            jle BOOK_IN_SHOP2
-            add word ptr ga2[16],word ptr 1
-            BOOK_IN_SHOP2:
-            mov ax,word ptr gb2[14]
-            cmp ax,word ptr gb2[16]
-            jle N_IN_SHOP1
-            add word ptr gb2[16],word ptr 1
-
-            N_IN_SHOP1:
-            mov ax,word ptr gaN[14];进货总数
-            cmp ax,word ptr gaN[16];判断进货总数是否大于已售数量
-            jle N_IN_SHOP2
-            add word ptr gaN[16],word ptr 1
-            N_IN_SHOP2:
-            mov ax,word ptr gbN[14]
-            cmp ax,word ptr gbN[16]
-            jle CALCULATE_PR
-            add word ptr gbN[16],word ptr 1
-
             mov goods_offset,0
-
-            ;计算利润率并存入存储单元
-            CALCULATE_PR:
-            mov si,goods_offset
-
-            MOV AX,WORD PTR ga1[si+10];进货价
-            IMUL ga1[si+14];AX现为cost1
+is_goods_cycle:
+            mov cx,10
+            mov si,0
+            mov bx,goods_offset
+            
+is_goods_cmp:
+            mov al,ga1[bx+si]
+            cmp al,goods_name[si+2]
+            jne next_goods
+            inc si
+            dec cx
+            jnz is_goods_cmp
+            
+            mov goods_name[bx+si+2],'$'
+            mov ax,word ptr ga1[bx+14]
+            cmp ax,word ptr ga1[bx+16]
+            jle GOODS_SOLD_OUT
+            add word ptr ga1[si+16],1
+            
+CALCULATE_PR:
+            MOV AX,WORD PTR ga1[bx+10];???
+            IMUL ga1[bx+14];AX??cost1
             MOV COST1,AX
-            MOV AX,WORD PTR ga1[si+12];销售价
-            IMUL ga1[si+16]
-            SUB AX,COST1;AX现为profit1
+            MOV AX,WORD PTR ga1[bx+12];???
+            IMUL ga1[bx+16]
+            SUB AX,COST1;AX??profit1
             MOV PROFIT1,AX
 
-            MOV AX,WORD PTR gb1[si+10];进货价
-            IMUL gb1[si+14];AX现为cost2
+            MOV AX,WORD PTR gb1[bx+10];???
+            IMUL gb1[bx+14];AX??cost2
             MOV COST2,AX
-            MOV AX,WORD PTR gb1[si+12];销售价
-            IMUL gb1[si+16]
-            SUB AX,COST2;AX现为profit2
+            MOV AX,WORD PTR gb1[bx+12];???
+            IMUL gb1[bx+16]
+            SUB AX,COST2;AX??profit2
             MOV PROFIT2,AX
 
             mov dx,0
@@ -214,29 +228,27 @@ FUNCTION3:
             IMUL AX,WORD PTR 10
             cwd
             IDIV COST1
-            mov word ptr ga1[si+18],ax;填充利润率
+            mov word ptr ga1[bx+18],ax;?????
             mov dx,0
             MOV AX,PROFIT2
             IMUL AX,WORD PTR 10
             cwd
             IDIV COST2
-            mov word ptr ga2[si+18],ax;填充利润率
-
+            mov word ptr ga2[bx+18],ax;?????
+            
+next_goods:
             add goods_offset,20
-            cmp goods_offset,40
-            jle CALCULATE_PR;小于或等于40继续计算
+            cmp goods_offset,600
+            jne is_goods_cycle
 
             sub cycle_times,1
-            jnz FUNCTION3;FUNCTION3循环继续
+            jnz FUNCTION3;FUNCTION3????
 mov ax,1
 call TIMER
             jmp FUNCION1
 
 GOODS_SOLD_OUT:
-            lea dx,goods_sold_out_msg
-            mov ah,9
-            int 21h
-            jmp FUNCION1
+            jmp next_goods
 
 EXIT:
             LEA DX,END_MSG
@@ -247,65 +259,65 @@ EXIT:
             MOV AH,4CH
             INT 21H
 
-            ;时间计数器(ms),在屏幕上显示程序的执行时间(ms)
-            ;使用方法:
-            ;	   MOV  AX, 0	;表示开始计时
-            ;	   CALL TIMER
-            ;	   ... ...	;需要计时的程序
-            ;	   MOV  AX, 1
-            ;	   CALL TIMER	;终止计时并显示计时结果(ms)
-            ;输出: 改变了AX和状态寄存器
-            TIMER	PROC
-            	PUSH  DX
-            	PUSH  CX
-            	PUSH  BX
-            	MOV   BX, AX
-            	MOV   AH, 2CH
-            	INT   21H	     ;CH=hour(0-23),CL=minute(0-59),DH=second(0-59),DL=centisecond(0-100)
-            	MOV   AL, DH
-            	MOV   AH, 0
-            	IMUL  AX,AX,1000
-            	MOV   DH, 0
-            	IMUL  DX,DX,10
-            	ADD   AX, DX
-            	CMP   BX, 0
-            	JNZ   _T1
-            	MOV   CS:_TS, AX
-            _T0:	POP   BX
-            	POP   CX
-            	POP   DX
-            	RET
-            _T1:	SUB   AX, CS:_TS
-            	JNC   _T2
-            	ADD   AX, 60000
-            _T2:	MOV   CX, 0
-            	MOV   BX, 10
-            _T3:	MOV   DX, 0
-            	DIV   BX
-            	PUSH  DX
-            	INC   CX
-            	CMP   AX, 0
-            	JNZ   _T3
-            	MOV   BX, 0
-            _T4:	POP   AX
-            	ADD   AL, '0'
-            	MOV   CS:_TMSG[BX], AL
-            	INC   BX
-            	LOOP  _T4
-            	PUSH  DS
-            	MOV   CS:_TMSG[BX+0], 0AH
-            	MOV   CS:_TMSG[BX+1], 0DH
-            	MOV   CS:_TMSG[BX+2], '$'
-            	LEA   DX, _TS+2
-            	PUSH  CS
-            	POP   DS
-            	MOV   AH, 9
-            	INT   21H
-            	POP   DS
-            	JMP   _T0
-            _TS	DW    ?
-             	DB    0AH,0DH,'Time elapsed in ms is '
-            _TMSG	DB    12 DUP(0)
+            ;?????(ms),?????????????(ms)
+            ;????:
+            ;      MOV  AX, 0   ;??????
+            ;      CALL TIMER
+            ;      ... ...  ;???????
+            ;      MOV  AX, 1
+            ;      CALL TIMER   ;???????????(ms)
+            ;??: ???AX??????
+            TIMER   PROC
+                PUSH  DX
+                PUSH  CX
+                PUSH  BX
+                MOV   BX, AX
+                MOV   AH, 2CH
+                INT   21H        ;CH=hour(0-23),CL=minute(0-59),DH=second(0-59),DL=centisecond(0-100)
+                MOV   AL, DH
+                MOV   AH, 0
+                IMUL  AX,AX,1000
+                MOV   DH, 0
+                IMUL  DX,DX,10
+                ADD   AX, DX
+                CMP   BX, 0
+                JNZ   _T1
+                MOV   CS:_TS, AX
+            _T0:    POP   BX
+                POP   CX
+                POP   DX
+                RET
+            _T1:    SUB   AX, CS:_TS
+                JNC   _T2
+                ADD   AX, 60000
+            _T2:    MOV   CX, 0
+                MOV   BX, 10
+            _T3:    MOV   DX, 0
+                DIV   BX
+                PUSH  DX
+                INC   CX
+                CMP   AX, 0
+                JNZ   _T3
+                MOV   BX, 0
+            _T4:    POP   AX
+                ADD   AL, '0'
+                MOV   CS:_TMSG[BX], AL
+                INC   BX
+                LOOP  _T4
+                PUSH  DS
+                MOV   CS:_TMSG[BX+0], 0AH
+                MOV   CS:_TMSG[BX+1], 0DH
+                MOV   CS:_TMSG[BX+2], '$'
+                LEA   DX, _TS+2
+                PUSH  CS
+                POP   DS
+                MOV   AH, 9
+                INT   21H
+                POP   DS
+                JMP   _T0
+            _TS DW    ?
+                DB    0AH,0DH,'Time elapsed in ms is '
+            _TMSG   DB    12 DUP(0)
             TIMER   ENDP
 
 CODE ENDS
