aff667404c479d108f2f78fc0cc46b34ff424f92
DragonDriver <longjiquan1998@163.com>
2018-04-29 15:05:42 +0800

    有用的批处理

---
+++ autocopy.bat
@@ -0,0 +1,17 @@
+@echo off
+
+for /d %%i in (Exp*) do (
+echo %%i
+cd %%i
+if exist CS1603_U201614577_龙际全*.zip (del CS1603_U201614577_龙际全*.zip)
+mkdir CS1603_U201614577_龙际全
+copy /y task*.* CS1603_U201614577_龙际全\
+rem this is for exp3/task1
+copy /y *ongj*.* CS1603_U201614577_龙际全\
+copy /y *U201614577*.doc* CS1603_U201614577_龙际全\
+7z a CS1603_U201614577_龙际全.zip CS1603_U201614577_龙际全\
+rmdir /s/q CS1603_U201614577_龙际全
+git add CS1603*.doc*
+git commit -m '实验报告'
+cd ..
+)
