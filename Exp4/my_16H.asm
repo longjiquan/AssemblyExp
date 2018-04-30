c722655757c548e08a1d7c7f7363aeb4af8e7027
DragonDriver <longjiquan1998@163.com>
2018-04-30 19:27:26 +0800

    修改后符合题目要求

--- Exp4/my_16H.asm
+++ Exp4/my_16H.asm
@@ -5,22 +5,32 @@ CODE   SEGMENT USE16
        ASSUME CS:CODE,ss:STACK
 OLD_INT DW ?,?
 
+;子程序名：	my_16H
+;功能：		重新接管16H中断，
+;			使键盘键入的小写字母变成大写字母，
+;			大写字母不变.
+;入口参数：	ax，ah为中断选择类型，al为字符
+;出口参数：	无
+;知识点（来源出处）：教材P222-P223
+;作者：boyjqlong@foxmail.com
 my_16H:cmp AH,00H
        JE  case_change
        cmp AH,10H
        JE  case_change
        JMP DWORD PTR OLD_INT
-case_change:
+case_change:;大小写转换
        PUSHF
        CALL DWORD PTR OLD_INT
+	   ;若想要完整的大小写转换，可以注释下一行
+	   jmp case_change_s_to_l;直接跳转
        cmp al,'Z';大于Z为小写字母
        jg case_change_s_to_l
-case_change_l_to_s:
+case_change_l_to_s:;大写变小写
         cmp al,'A'
         jl case_change_QUIT
         add al,20h;大小变大写
         jmp case_change_QUIT
-case_change_s_to_l:
+case_change_s_to_l:;小写变大写
         cmp al,'a'
         jl case_change_QUIT
         cmp al,'z'
